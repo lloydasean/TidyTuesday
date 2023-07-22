139aaa2da628a608f7743677addb1418a06e996d
Sean Lloyd <lloydsean@outlook.com>
2023-07-22 15:25:22 +0100

    Update GPT Detectors.R

--- 2023/18_07/GPT Detectors.R
+++ 2023/18_07/GPT Detectors.R
@@ -1,7 +1,6 @@
 library(tidytuesdayR)
 library(tidyverse)
 library(ggplot2)
-library(ggridges)
 
 #load data
 tuesdata <- tidytuesdayR::tt_load('2023-07-18')
@@ -12,7 +11,7 @@ detectors <- tuesdata$detectors
 #explore data
 head(detectors)
 
-#was model prediction correct?
+#was detector prediction correct?
 mutate(detectors,correct=0) 
 detectors$correct[detectors$.pred_class==detectors$kind] <- TRUE
 detectors$correct[detectors$.pred_class!=detectors$kind] <- FALSE
@@ -25,8 +24,8 @@ detector_accuracy <- detectors %>%
 detector_accuracy %>% ggplot(aes(y=percent_correct,x=reorder(detector,percent_correct)))+
   geom_col(width=0.5, aes(fill=percent_correct))+
   scale_y_continuous(labels = scales::percent)+
-  scale_fill_gradient("Accuracy",low = "#ff9696",high = "#b8ff96",labels=scales::percent)+
-  labs(title="AI Detector Accuracy",x="Detector",y=NULL)+
+  scale_fill_gradient("Accuracy",low = "#ff9696",high = "#c8ff96",labels=scales::percent)+
+  labs(title="GPT Detector Accuracy",x="Detector",y=NULL)+
   theme(axis.text.x=element_text(angle=50,vjust=0.6))
 
 
@@ -39,7 +38,7 @@ model_undetectable <- detectors %>%
 model_undetectable %>% ggplot(aes(y=percent_undetected,x=model))+
   geom_col(width=0.5)+
   scale_y_continuous(labels=scales::percent)+
-  labs(title="GPT3 vs GPT4",x="Model",y=NULL, subtitle="Percentage of essays written by AI that are undetected")
+  labs(title="GPT3 vs GPT4",x="Model",y=NULL, subtitle="Percentage of essays written by GPT that go undetected")
 
 #native vs non-native English speakers 
 false_positives <- detectors %>% 
@@ -50,6 +49,6 @@ false_positives <- detectors %>%
 false_positives %>% ggplot(aes(y=percent_correct,x=detector,fill=native))+
   geom_bar(stat="identity",position="dodge",width=0.5)+
   scale_y_continuous(labels=scales::percent)+
-  labs(fill="Native English Speaker",title="Percentage of Correct Detections",subtitle="Native vs Non-Native English Spekaers",x="Detector",y=NULL)+
+  labs(fill="Native English Speaker",title="GPT Detector Accuracy",subtitle="Native vs Non-Native English Spekaers",x="Detector",y=NULL)+
   theme(axis.text.x=element_text(angle=50,vjust=0.6))
 
